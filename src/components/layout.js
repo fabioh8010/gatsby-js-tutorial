ad9973a3fdd9630e080d9d5b66e350c038890cb2
Fábio Henriques <fabio.henriques@devoteam.com>
2022-08-15 21:20:50 +0100

    Finished Gatsby Tutorial Part 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,51 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
   navLinkText,
+  siteTitle,
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
 
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
-            <Link className={navLinkText} to="/">Home</Link>
+            <Link className={navLinkText} to="/">
+              Home
+            </Link>
           </li>
 
           <li className={navLinkItem}>
-            <Link className={navLinkText} to="/about">About</Link>
+            <Link className={navLinkText} to="/about">
+              About
+            </Link>
+          </li>
+
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/blog">
+              Blog
+            </Link>
           </li>
         </ul>
       </nav>
